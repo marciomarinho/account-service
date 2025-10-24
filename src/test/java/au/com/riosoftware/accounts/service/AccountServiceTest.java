482362f3b0dcb8b84dc92a90e8e493b70a26b8a0
Marcio Marinho <marciomarinho@gmail.com>
2025-10-24 17:14:06 +1100

    Adding unit tests to controller account

---
+++ src/test/java/au/com/riosoftware/accounts/service/AccountServiceTest.java
@@ -0,0 +1,20 @@
+package au.com.riosoftware.accounts.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountServiceTest {
+
+    @Test
+    void testCreateAccount() {
+    }
+
+    @Test
+    void testFromRequest() {
+    }
+
+    @Test
+    void testFindAll() {
+    }
+}
