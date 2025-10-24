85138407094d531430f32240016b20a066319334
Marcio Marinho <marciomarinho@gmail.com>
2025-10-24 17:56:40 +1100

    Testing account service

--- src/test/java/au/com/riosoftware/accounts/service/AccountServiceTest.java
+++ src/test/java/au/com/riosoftware/accounts/service/AccountServiceTest.java
@@ -1,13 +1,24 @@
 package au.com.riosoftware.accounts.service;
 
+import au.com.riosoftware.accounts.repository.AccountRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class AccountServiceTest {
 
+    private AccountRepository accountRepository;
+
+    @BeforeEach
+    void setUp() {
+        accountRepository = mock(AccountRepository.class);
+    }
+
     @Test
     void testCreateAccount() {
+
     }
 
     @Test
