7fdc0ce02607f67585d66bce6d9693a7b2714031
Marcio Marinho <marciomarinho@gmail.com>
2025-10-25 17:23:59 +1100

    Refactoring & Adding README.md

--- src/main/java/au/com/riosoftware/accounts/Application.java
+++ src/main/java/au/com/riosoftware/accounts/Application.java
@@ -2,7 +2,6 @@ package au.com.riosoftware.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
 public class Application {
